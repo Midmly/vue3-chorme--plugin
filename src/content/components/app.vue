cc233a1d737a3419c706cab9a9088ff95a3d9001
medy <1228022817@qq.com>
2023-04-25 20:49:00 +0800

    first commit

---
+++ src/content/components/app.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="head">太酷啦</div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.head{
+	color: red;
+	position: fixed;
+	z-index: 100001;
+	right: 10px;
+	bottom: 10px;
+}
+</style>
